3fdf84f67b7787042f25110bffbc48630c5d5230
jiangtao <jiangtao@wepiao.com>
2016-03-23 10:50:10 +0800

    update

--- src/start.js
+++ src/start.js
@@ -1,5 +1,21 @@
 var Vue       = require('vue');
 var VueRouter = require('vue-router');
+var vueConfig = require('./router-config');
+
+Vue.use(VueRouter);
+
+var App = require('./template/app.vue');
+
+var router = new VueRouter();
+
+
+
+vueConfig(router)
+
+
+
+
+router.start(App, '#app');
 
 
 
